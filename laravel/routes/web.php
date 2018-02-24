413d847b2580b8053203dc2d00ad743ca5211d4a
Víctor López <v.lopezarcas@gmail.com>
2018-02-24 23:20:51 +0100

    no message

---
+++ laravel/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return 'All cats';
+
+    // If we wanted users to be redirected to /cats when they visit the application for the first time
+    // return redirect('cats');
+});
+
+Route::get('cats', function() {
+	return 'All cats';
+});
+
+Route::get('cats/{id}', function($id) {
+	return sprintf('Cat #%s', $id);
+})->where('id', '[0-9]+');
+
+Route::get('about', function() {
+	return view('about')->with('number_of_cats', 9000);
+});
+
+// Laravel 5 Essentials page 28. - Preparing the database
